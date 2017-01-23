0c0896c2f2f54bf9b092134c320c31f6e5ce7db5
tobynet <toby.netinfo+git@gmail.com>
2017-01-24 02:24:13 +0900

    Implement first classifyOxtuData

--- src/state.ts
+++ src/state.ts
@@ -40,6 +40,7 @@ export let datasets: {[key: string]: dataset.DataGenerator} = {
   "xor": dataset.classifyXORData,
   "gauss": dataset.classifyTwoGaussData,
   "spiral": dataset.classifySpiralData,
+  "oxtu": dataset.classifyOxtuData,
 };
 
 /** A map between dataset names and functions that generate regression data. */
